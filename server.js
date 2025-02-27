b415590e07b6c1f6ae4ac4da39f2914fefa884c2
m7md1221 <derghamdawa@gmail.com>
2025-02-27 21:50:18 +0200

    fiin

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'index.html')));
 
 
 app.get('/', (req, res) => {
