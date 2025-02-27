982d4f1d17c45c43e43a57213a49138ee2856f24
m7md1221 <derghamdawa@gmail.com>
2025-02-27 23:29:04 +0200

    fii1n

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const path = require('path');
-const fs = require('fs'); // استيراد fs لقراءة ملفات النظام
+const fs = require('fs'); 
 
 app.use(express.static(path.join(__dirname, 'public')));
 
